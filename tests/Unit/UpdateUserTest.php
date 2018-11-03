926fe75332d2e57f9a9c6aa6599163f810b7fcf7
kalp97 <kdd32@njit.edu>
2018-11-03 17:00:16 -0400

    Task: Unit test to update a user to Steve Smith

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tests\Unit;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUpdateUser()
+    {
+        $user = User::find(5);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $this->assertDatabaseHas('users', ["name" => "Steve Smith"]);
+    }
+}
