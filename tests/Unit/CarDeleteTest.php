c0a1f8b613bd6b952902f1473f6fe3baba9318b5
kalp97 <kdd32@njit.edu>
2018-11-03 17:34:41 -0400

    Task: Unit test to delete a car in database

---
+++ tests/Unit/CarDeleteTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Unit;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+class CarDeleteTest extends TestCase
+{
+    /**
+     */
+    public function testDeletecar()
+    {
+        $car = Car::find(51);
+        DB::table('cars')->where('id', '>', 50)->delete();
+        $cars = Car::all();
+        $this->assertDatabaseMissing('cars', ['id' => '51']);
+    }
+}
